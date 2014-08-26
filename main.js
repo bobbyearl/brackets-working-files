c9a809df4ee2fa33744d09344f5804eaaf58cf14
Bobby Earl <bobby@simplyearl.com>
2014-08-26 09:29:27 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+
+/**
+* Simple extension to remove the max-height rule on the Working Files area.
+*
+* Bobby Earl
+* bobby@simplyearl.com
+**/
+define(function (require, exports, module) {
+    "use strict";
+
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        Menus = brackets.getModule("command/Menus"),
+		    PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
+    	  prefs = PreferencesManager.getExtensionPrefs("tallerWorkingFiles"),
+		    menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU),
+		    commandId = 'bobbyearl.taller-working-files',
+		    prefKey = 'taller';
+	
+	  // Create our handler
+    CommandManager.register("Taller Working Files", commandId, function() {
+		  prefs.set(prefKey, !prefs.get(prefKey));
+		  prefs.save();
+	  });
+	
+	  // Add our item to the menu
+    menu.addMenuItem(commandId);
+	
+	// Set the state of the working files area when our preference is changed
+	prefs.definePreference(prefKey, 'boolean', 'false').on('change', function() {
+		$('#open-files-container').css('max-height', prefs.get(prefKey) ? '' : '200px');
+	});
+});
